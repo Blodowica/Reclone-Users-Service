f73c0f0aac3b157d20e39b28cb28e93893467d21
Brandon Lodowica <brandonlodowica@hotmail.com>
2023-04-20 17:03:28 +0200

    Update PostController.cs

--- Reclone-Users-Service/Reclone-Users-Service/Controllers/PostController.cs
+++ Reclone-Users-Service/Reclone-Users-Service/Controllers/PostController.cs
@@ -7,5 +7,34 @@ namespace Reclone_Users_Service.Controllers
     [ApiController]
     public class PostController : ControllerBase
     {
+        private readonly HttpClient _httpClient;
+
+
+
+        public PostController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient("PostMicroservice");
+        }
+
+        //establihs get connect with the post microservice
+
+        [HttpGet("getPostServiceResponse")]
+        public async Task<string> Get()
+        {
+            var response = await _httpClient.GetAsync("/api/user/sendPostServiceResponse");
+            return await response.Content.ReadAsStringAsync();
+        }
+
+
+        //return response to post microservice
+        [HttpGet("sendUserServiceResponse")]
+        public string GetResponse()
+        {
+            return "Hello from User Microservice!";
+        }
+
     }
+
+
+
 }
